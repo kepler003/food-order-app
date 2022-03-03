5e1b1200caf002d9bd5d192947b3e2044c72e48b
Bartek <b.pyzocha@gmail.com>
2022-03-03 23:27:34 +0100

    Add Icon component

---
+++ src/UI/Icon.js
@@ -0,0 +1,21 @@
+import cls from './Icon.module.css';
+import cartIcon from '../icons/cart.png';
+
+const Icon = ({ className, name, color }) => {
+  const icons = new Map([['cart', cartIcon]]);
+
+  if (!icons.has(name)) {
+    throw Error(`Icon "${name}" doesn't exist.`);
+  }
+
+  return (
+    <span className={`${cls.box} ${className}`}>
+      <img
+        className={`${cls.img} ${color === 'white' ? cls.white : ''}`}
+        src={icons.get(name)}
+      />
+    </span>
+  );
+};
+
+export default Icon;
