15428885e5271f5ebcb16204b39dd226a15ebf5e
Bartek <b.pyzocha@gmail.com>
2022-03-04 00:05:32 +0100

    Add Button component

--- src/UI/Button/Button.js
+++ src/UI/Button/Button.js
@@ -1,14 +1,26 @@
 import cls from './Button.module.css';
 
 const Button = (props) => {
-  const { children } = props;
-  props = {
-    ...props,
-    className: props.className + ' ' + cls.button,
-    children: undefined
+  const { children, btnType = 'primary' } = props;
+
+  const btnTypes = new Map([
+    ['primary', ''],
+    ['primary-dark', cls.buttonPrimaryDark],
+    ['primary-darker', cls.buttonPrimaryDarker],
+  ]);
+
+  if (!btnTypes.has(btnType)) {
+    throw new Error(`Btn "${btnType}" doesn't exist.`);
   }
 
-  return <button {...props}>{ children }</button>
+  props = {
+    ...props,
+    className: props.className + ' ' + cls.button + ' ' + btnTypes.get(btnType),
+  };
+  delete props.children;
+  delete props.btnType;
+
+  return <button {...props}>{children}</button>;
 };
 
 export default Button;
