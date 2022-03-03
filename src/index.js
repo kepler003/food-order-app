0f5cdb1c36ed2dc62c70257e1154410cb6628b83
Bartek <b.pyzocha@gmail.com>
2022-03-03 22:55:33 +0100

    Add css variables

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import './reset.css';
+import './css/index.css';
+import './css/variables.css';
+import './css/reset.css';
 import App from './App';
 
 ReactDOM.render(
