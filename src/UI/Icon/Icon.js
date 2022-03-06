0766f35ef5727e02870803e4b99c92cf7299e818
Bartek <b.pyzocha@gmail.com>
2022-03-06 14:47:29 +0100

    Refactoring

--- src/UI/Icon/Icon.js
+++ src/UI/Icon/Icon.js
@@ -1,19 +1,36 @@
 import cls from './Icon.module.css';
 import cartIcon from '../../icons/cart.png';
+import checkIcon from '../../icons/check.png';
+import plusIcon from '../../icons/plus.png';
+import minusIcon from '../../icons/minus.png';
 
-const Icon = ({ className, name, color }) => {
-  const icons = new Map([['cart', cartIcon]]);
+const Icon = (props) => {
+  const { className, name, color } = props;
+
+  props = {
+    ...props,
+    className: className + ' ' + cls.box,
+  };
+
+  delete props.name;
+  delete props.color;
+
+  const icons = new Map([
+    ['cart', cartIcon],
+    ['check', checkIcon],
+    ['plus', plusIcon],
+    ['minus', minusIcon],
+  ]);
 
   if (!icons.has(name)) {
     throw Error(`Icon "${name}" doesn't exist.`);
   }
 
+  const imgClass = color === 'white' ? cls.white : '';
+
   return (
-    <span className={`${cls.box} ${className}`}>
-      <img
-        className={`${cls.img} ${color === 'white' ? cls.white : ''}`}
-        src={icons.get(name)}
-      />
+    <span {...props}>
+      <img className={[cls.img, imgClass].join(' ')} src={icons.get(name)} />
     </span>
   );
 };
