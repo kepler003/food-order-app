70b8f3ee3919064a3995460f1c6ec64eadc4db72
Bartek <b.pyzocha@gmail.com>
2022-03-07 22:01:59 +0100

    Add jumping animation

--- src/components/CartBtn/CartBtn.js
+++ src/components/CartBtn/CartBtn.js
@@ -1,10 +1,18 @@
 import cls from './CartBtn.module.css';
 import Icon from '../../UI/Icon/Icon';
 import Button from '../../UI/Button/Button';
+import { useEffect, useState } from 'react';
 
 const CartBtn = ({ numOfItems }) => {
+  const [jump, setJump] = useState(false);
+
+  useEffect(() => {
+    setJump(true);
+    setTimeout(() => setJump(false), 150);
+  }, [numOfItems]);
+
   return (
-    <Button className={cls.btn} variant='primary-darker'>
+    <Button className={[cls.btn, jump ? cls.jump : ''].join(' ')} variant='primary-darker'>
       <Icon className={cls.icon} name='cart' color='white' />
       <span className={cls.label}>Your Cart</span>
       <span className={cls.number}>{numOfItems}</span>
