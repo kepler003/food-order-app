1d570a1407837927b315f39463578afa07c1f6f4
Bartek <b.pyzocha@gmail.com>
2022-03-07 21:47:05 +0100

    Use menu context for displaying items amount

--- src/components/CartBtn/CartBtn.js
+++ src/components/CartBtn/CartBtn.js
@@ -2,12 +2,12 @@ import cls from './CartBtn.module.css';
 import Icon from '../../UI/Icon/Icon';
 import Button from '../../UI/Button/Button';
 
-const CartBtn = () => {
+const CartBtn = ({ numOfItems }) => {
   return (
     <Button className={cls.btn} variant='primary-darker'>
       <Icon className={cls.icon} name='cart' color='white' />
       <span className={cls.label}>Your Cart</span>
-      <span className={cls.number}>0</span>
+      <span className={cls.number}>{numOfItems}</span>
     </Button>
   );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,22 @@
 import cls from './Header.module.css';
 import CartBtn from '../CartBtn/CartBtn';
+import { useContext } from 'react';
+import MenuContext from '../../store/MenuContext';
 
 const Header = () => {
+  const menuCtx = useContext(MenuContext);
+  const numOfItems = menuCtx.order.reduce(
+    (prev, curr) => prev + curr.amount,
+    0
+  );
+
   return (
     <header className={cls.header}>
       <div className='row'>
-        <a className={cls.logo} href='#'>ReactMeals</a>
-        <CartBtn />
+        <a className={cls.logo} href='#'>
+          ReactMeals
+        </a>
+        <CartBtn numOfItems={numOfItems} />
       </div>
     </header>
   );
