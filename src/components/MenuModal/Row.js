27befc3c8867cd357a409286b52d465f923fc8ff
Bartek <b.pyzocha@gmail.com>
2022-03-07 23:16:38 +0100

    Add Row component

---
+++ src/components/MenuModal/Row.js
@@ -0,0 +1,33 @@
+import Button from '../../UI/Button/Button';
+import Icon from '../../UI/Icon/Icon';
+import cls from './Row.module.css';
+
+const Row = ({ name, price, amount, onAdd, onRemove }) => {
+  const addHandler = () => {
+    onAdd({ name });
+  }
+
+  const removeHandler = () => {
+    onRemove({ name });
+  }
+  
+  return (
+    <div className={cls.row}>
+      <div>
+        <p className={cls.name}>{name}</p>
+        <p className={cls.price}>${price / 100}</p>
+      </div>
+      <div className={cls.controlBtns}>
+        <p className={cls.amount}>x{amount}</p>
+        <Button className={cls.controlBtn} size='small' onClick={removeHandler}>
+          <Icon name='minus' color='white' />
+        </Button>
+        <Button className={cls.controlBtn} size='small' onClick={addHandler }>
+          <Icon name='plus' color='white' />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Row;
